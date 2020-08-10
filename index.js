849fb2873d9a01106fdf65aac0f74aaf4bf5e290
Anwesh43 <anweshthecool0@gmail.com>
2020-08-11 04:06:18 +0530

    adding DrawingUtil class

--- index.js
+++ index.js
@@ -5,3 +5,13 @@ const strokeFactor= 90
 const sizeFactor = 2.9
 const rot = Math.PI / 2
 const hFactor = 3
+
+class DrawingUtil {
+
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+}
