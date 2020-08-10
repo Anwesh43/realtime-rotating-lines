226b6d1c388e9938a8ce314b6b7e3eacaeeeb2ad
Anwesh43 <anweshthecool0@gmail.com>
2020-08-11 04:09:26 +0530

    adding State class

--- index.js
+++ index.js
@@ -15,3 +15,27 @@ class DrawingUtil {
         context.stroke()
     }
 }
+
+class State {
+
+    scale = 0
+    dir = 0
+    prevScale = 0
+
+    update(cb) {
+        this.scale += scGap * this.dir
+        if (Math.abs(this.scale - this.prevScale) > 1) {
+            this.scale = this.prevScale + this.dir
+            this.dir = 0
+            this.prevScale = this.scale
+            cb()
+        }
+    }
+
+    startUpdating(cb) {
+        if (this.dir == 0) {
+            this.dir = 1 - 2 * this.prevScale
+            cb()
+        }
+    }
+}
